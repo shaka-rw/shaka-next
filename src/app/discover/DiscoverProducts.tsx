eddfd148e30ca926c4387bc7af1b8902601ff9fb
phanuelnd <ndikumanaphanuel@gmail.com>
2023-09-29 14:27:59 +0200

    Attempt to fix no-approved in discover

--- src/app/discover/DiscoverProducts.tsx
+++ src/app/discover/DiscoverProducts.tsx
@@ -13,8 +13,8 @@ const DiscoverProducts = async ({
     return value === '-Infinity'
       ? -Infinity
       : value === 'Infinity'
-      ? Infinity
-      : parseFloat(value);
+        ? Infinity
+        : parseFloat(value);
   }
 
   const {
@@ -33,86 +33,84 @@ const DiscoverProducts = async ({
     searchType !== 'products'
       ? []
       : await prisma.category.findMany({
-          take: 6,
-          // skip: parseInt(_page ?? '0') * 30,
-          include: {
-            products: {
-              take: 5,
-              where: {
-                AND: {
-                  quantities: {
-                    some: {
-                      AND: {
-                        quantity: { gt: 0 },
-                        ...(price
-                          ? {
-                              price: {
-                                gte: isNaN(minPrice) ? undefined : minPrice,
-                                lte: isNaN(maxPrice) ? undefined : maxPrice,
-                              },
-                            }
-                          : {}),
-                      },
-                    },
-                  },
-                  ...(search
-                    ? {
-                        OR: [
-                          { name: { contains: search, mode: 'insensitive' } },
-                          {
-                            description: {
-                              contains: search,
-                              mode: 'insensitive',
-                            },
-                          },
-                        ],
-                      }
-                    : {}),
-                  available: true,
-                  ...(gender
-                    ? { gender: { equals: gender as ProductGender } }
-                    : {}),
-                  ...(size
-                    ? {
-                        sizes: {
-                          some: { size: { equals: size, mode: 'insensitive' } },
+        take: 6,
+        // skip: parseInt(_page ?? '0') * 30,
+        include: {
+          products: {
+            take: 5,
+            where: {
+              shop: { approved: true },
+              quantities: {
+                some: {
+                  AND: {
+                    quantity: { gt: 0 },
+                    ...(price
+                      ? {
+                        price: {
+                          gte: isNaN(minPrice) ? undefined : minPrice,
+                          lte: isNaN(maxPrice) ? undefined : maxPrice,
                         },
                       }
-                    : {}),
-                  ...(categoryId
-                    ? {
-                        OR: [
-                          { categories: { some: { id: categoryId } } },
-                          { shop: { category: { id: categoryId } } },
-                        ],
-                      }
-                    : {}),
-                  // categories: { some: { id: catId || undefined } },
-                },
-              },
-              include: {
-                mainImage: true,
-                shop: { include: { image: true, category: true } },
-                categories: { include: { image: true } },
-                quantities: {
-                  where: { quantity: { gt: 0 } },
-                  distinct: ['productColorId', 'productSizeId'],
-                  include: {
-                    color: { include: { mainImage: true } },
-                    size: true,
+                      : {}),
                   },
                 },
-                sizes: true,
-                colors: { include: { mainImage: true, images: true } },
               },
+              ...(search
+                ? {
+                  OR: [
+                    { name: { contains: search, mode: 'insensitive' } },
+                    {
+                      description: {
+                        contains: search,
+                        mode: 'insensitive',
+                      },
+                    },
+                  ],
+                }
+                : {}),
+              available: true,
+              ...(gender
+                ? { gender: { equals: gender as ProductGender } }
+                : {}),
+              ...(size
+                ? {
+                  sizes: {
+                    some: { size: { equals: size, mode: 'insensitive' } },
+                  },
+                }
+                : {}),
+              ...(categoryId
+                ? {
+                  OR: [
+                    { categories: { some: { id: categoryId } } },
+                    { shop: { category: { id: categoryId } } },
+                  ],
+                }
+                : {}),
+            },
+            include: {
+              mainImage: true,
+              shop: { include: { image: true, category: true } },
+              categories: { include: { image: true } },
+              quantities: {
+                where: { quantity: { gt: 0 } },
+                distinct: ['productColorId', 'productSizeId'],
+                include: {
+                  color: { include: { mainImage: true } },
+                  size: true,
+                },
+              },
+              sizes: true,
+              colors: { include: { mainImage: true, images: true } },
             },
           },
-          orderBy: {
-            products: {
-              _count: 'desc',
-            },
+        },
+        orderBy: {
+          products: {
+            _count: 'desc',
           },
-        });
+        },
+      });
 
   return (
     <div className="mx-auto container p-1">
